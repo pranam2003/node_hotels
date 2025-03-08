35d4c99a5fa9414e33e6f4b04e56bbd274216508
pranam2003 <pranpranam@gmail.com>
2025-03-08 16:45:27 +0530

    final

--- server.js
+++ server.js
@@ -1,71 +1,90 @@
-// function add(a, b)
+const express=require('express');
+const app=express();
+const db=require('./db');
+// const Person=require('./models/Person'); 
+require('dotenv').config();
+const bodyparser=require('body-parser');
+app.use(bodyparser.json());  //req.body
+
+// app.get('/' ,function(req,res){
+//     res.send("hello welcome");
+// })
+// app.get('/chicken' ,(req,res)=>{
+//     res.send("i would love to server chicken");
+// })
+// app.get('/idli' ,(req,res)=>{
+//     var cust={
+//         name:"ravaidli",
+//         size:"10cm",
+
+//     }
+//     res.send(cust);
+// })
+// app.post('/Person',async (req,res)=>{
+//     try{
+//     // res.send("data is saved");
+//     const data = req.body // assuming the request body contains the person data
+//     const newperson= new Person(data);
+//     // newperson.name=data.name;
+//     // newperson.age=data.age;
+//     // newperson.mobile=data.mobile;
+//     // newperson.email=data.email;
+   
+//     const response= await newperson.save();
+//     console.log("data saved");
+//     res.status(200).json(response);
+
+//     }
+//     catch(err){
+//         console.log(err);
+//         res.status(500).json({error:"internal server error"});
+//     }
+// }) 
+// // getting person data
+// app.get('/person' , async(req,res)=>
 // {
-//     return a+b
-// }
-//   var add =function(a,b)
-//   {
-//     return a+b;
-//   }
-// var add=(a,b)=>
-//     {return a+b;}
-// var result=add(3,2)
-// console.log(result)
+//     try{
+//         const data= await Person.find()
+//         console.log("data fecth");
+//         res.status(200).json(data);
 
 
-// (function(a,b){
-// console.log("hello")}
-// )();
+//     }
+//         catch(err){
+//             console.log(err);
+//         res.status(500).json({error:"internal server error"});
 
-
-// ^ differnt types of writing functions
-
-
-// function callback()
+//         }
+    
+// })
+// app.get('/person/:workType',async(req,res)=>
 // {
-//     console.log("this is call back")
-// }
-// const add=function(a,b,callback)
-// {
-//     var result=a+b;
-//     console.log(result);
-//     callback();
+    
+//         try{
+//             const workType=req.params.workType;
+//     if(workType=="chef" || workType=="waiter"|| workType=="manager"){
+//         const data= await Person.find({work:workType})
+//         console.log('response is fetched');
+//         res.status(200).json(data);
 
-// }
-// add(21,12,callback);
+//         }else{
+//             res.status(404).json({error:'invalid work type'});
+//         }
+//     }
+//         catch(err)
+//         {
+// console.log(err);
+//         res.status(500).json({error:"internal server error"});
+        
+         
+//     }
+// });
 
-//2nd type call back function
-// const add=function(a,b,callback)
-// {
-//     var result=a+b;
-//     console.log(result);
-//     callback();
-
-// }
-// add(12,12,()=>console.log("helloo"));
-
-
-
-// var fs=require('fs')
-// var os=require('os')
-// var user=os.userInfo();
-// console.log(user.username);
-// fs.appendFile('simple.txt','hi'+user.username+'\n',()=>(console.log("file created")))
-
-// const notes = require('./notes.js');
-// var age=notes.age;
-// var r=notes.add(age,16)
-// console.log(age);
-// console.log(r);
-
-
-var _ = require('lodash');
-var data=['pranam','pranam',1,2,1,2];
-var filter=_.uniq(data);
-console.log(filter);
-console.log(_.isString(true));
-
-
-// json to obj convert// and obj to json
-const json='{"name":"John", "age":30, "city":"New York"}';
-const obj = JSON.parse(json);
-console.log(obj);
+const personRoutes=require('./routes/personRoutes');
+// use the routers
+app.use('/person',personRoutes); //person is used endpoint instead of using in routesperson like get postC
+const PORT =process.env.PORT || 3000;
+app.listen(PORT,()=>
+{
+    console.log("listening to port 3000")
+});
